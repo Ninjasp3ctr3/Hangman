e3c68e0932ed39296808a617e71869b22dbdfb35
ryan.barry <ryanmactavich@gmail.com>
2024-10-22 20:12:35 +0200

    New version of hangman with extras and a few optimization changes

--- Hangman.js
+++ Hangman.js
@@ -1,63 +1,77 @@
-const listOfWords = [
-  "Javascript",
-  "Codewars",
-  "Frontend",
-  "Laptop",
-  "Syntax",
-  "Hangman",
-];
+function start() {
+  const listOfWords = [
+    "Javascript",
+    "Codewars",
+    "Frontend",
+    "Laptop",
+    "Syntax",
+    "Hangman",
+  ];
 
-function pickRandomWord() {
-  let randomWord = Math.floor(Math.random() * listOfWords.length);
-  return randomWord;
-}
-
-const guessedLetters = [];
-let wrongGuesses = 0;
-const maxMistakes = 6;
-let guessedStatus = [];
-let wordToGuess = listOfWords[pickRandomWord()];
-let indices = [];
-
-console.log("word to guess: " + wordToGuess);
-
-function setGuessedStatus() {
-  for (i = 0; i < wordToGuess.length; i++) {
-    guessedStatus.push("_");
+  function pickRandomWord() {
+    let randomWord = Math.floor(Math.random() * listOfWords.length);
+    return listOfWords[randomWord].toLowerCase();
   }
-}
 
-setGuessedStatus();
+  const guessedLetters = [];
+  let wrongGuesses = 0;
+  const maxMistakes = 6;
+  let guessedStatus = [];
+  let wordToGuess = pickRandomWord();
 
-function displayStatus() {
-  console.log(guessedStatus.join(""));
-  console.log("Wrong guesses: " + wrongGuesses);
-}
-
-console.log(guessedStatus);
-
-while (wrongGuesses < 6 || guessedLetters.join("") == wordToGuess) {
-  displayStatus();
-  let guess = prompt("Guess a letter:");
-  if (!/^[a-z]$/.test(guess) || guessedLetters.includes(guess)) {
-    alert("Invalid guess!");
-  } else if (wordToGuess.includes(guess)) {
-    for (let i = 0; i < wordToGuess.length; i++) {
-      if (wordToGuess[i] === guess) {
-        guessedStatus[i] = guess;
-      }
-      console.log(guessedStatus);
+  function restart() {
+    let newGame = prompt("New game? (Y/N)").toLowerCase();
+    if (newGame === "y") {
+      start();
+    } else {
+      alert("See you next time!");
     }
-  } else {
-    alert("Wrong guess!");
-    wrongGuesses++;
+  }
+
+  console.log("word to guess: " + wordToGuess);
+
+  function setGuessedStatus() {
+    guessedStatus = Array(wordToGuess.length).fill("_");
+  }
+
+  function displayStatus() {
+    console.log(guessedStatus.join(""));
+    console.log("Wrong guesses: " + wrongGuesses);
+  }
+
+  setGuessedStatus();
+  console.log("word to guess: " + wordToGuess);
+
+  while (wrongGuesses < maxMistakes && guessedStatus.includes("_")) {
+    displayStatus();
+    let guess = prompt("Guess a letter:").toLowerCase();
+    if (!/^[a-z]$/.test(guess) || guessedLetters.includes(guess)) {
+      alert("Invalid guess!");
+    } else if (wordToGuess.includes(guess)) {
+      for (let i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i] === guess) {
+          guessedStatus[i] = guess;
+        }
+        guessedLetters.push(guess);
+      }
+    } else {
+      alert("Wrong guess!");
+      guessedLetters.push(guess);
+      wrongGuesses++;
+    }
+  }
+
+  if (wrongGuesses === maxMistakes) {
+    alert("You lost, the word was: " + wordToGuess);
+    restart();
+  } else if (!guessedStatus.includes("_")) {
+    alert("You guessed the word: " + wordToGuess);
+    restart();
   }
 }
 
-if (wrongGuesses == maxMistakes) {
-  alert("You lost, the word was: " + wordToGuess);
-} else if (!guessedStatus.includes("_")) {
-  alert("You guessed the word: " + wordToGuess);
-}
+start();
 
-console.log(wordToGuess.split(""));
+function addWord(newWord) {
+  listOfWords.push(newWord);
+}
